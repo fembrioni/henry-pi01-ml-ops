edbe6e51459d548acef4b33e0e9eab7aa5e6156b
fembrioni <fembrioni@gmail.com>
2023-06-07 20:39:05 +0000

    Endpoint 3

--- code/main.py
+++ code/main.py
@@ -58,5 +58,34 @@ def cantidad_filmaciones_dia(dia: str):
         q = m_df[m_df['release_day_of_week'] == dia_ingles].release_day_of_week.count()
         return '{} cantidad de películas fueron estrenadas en los dias {}'.format(str(q), dia)
 
+# Endpoint 3
+@fastAPIApp.get("/score_titulo/{titulo_de_la_filmacion}")
+def score_titulo(titulo_de_la_filmacion: str):
+    '''Se ingresa el título de una filmación esperando como respuesta el título, el año de estreno y el score.
+    Ejemplo de retorno: La película X fue estrenada en el año X con un score/popularidad de X
+    Se asume: 
+     a) el título será ingresado completo
+     b) el score se corresponde con el campo popularity'''
+
+    # Evaluo la cantidad de registros con ese nombre de pelicula
+    m_df = dataframes_d['m_df']
+    q = m_df[m_df['title'] == titulo_de_la_filmacion].title.count()
+
+    # Si no hay registros se informa tal situacion
+    if q < 1:
+        return 'No se encuentran registros con el siguiente nombre de filmación: {}'.format(titulo_de_la_filmacion)
+    else:
+        # Existe al menos un registro. Me quedo con el primero de ellos
+        registros = m_df[m_df['title'] == titulo_de_la_filmacion]
+        registro = registros.head(1)
+
+        # Obtengo los valores de release_year y popularity
+        release_year = registro['release_year'].values[0]
+        popularity = registro['popularity'].values[0]
+
+        # Respondo
+        return 'La película {} fue estrenada en el año {} con un score/popularidad de {}'\
+            .format(titulo_de_la_filmacion, release_year, popularity)
+
 if __name__ == "__main__":
     uvicorn.run(fastAPIApp, host="0.0.0.0", port=8000)
